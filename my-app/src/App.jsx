3bb666557a8be79f9c4eca79936656b51c2279cb
Giuseppe Crimi <giuseppecrimi1999@hotmail.com>
2023-12-06 16:48:49 +0100

    exercise of props1

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,11 @@
-import { Hello } from "./Hello";
+// import { Hello } from "./Hello";
+
+import { Welcome } from "./Welcome";
 
 export function App (){
-    return(
-        <div>
-        
-                 <Hello />
-            
-           
-        </div>
-    )
+    return <Welcome name="Luigi"/>        
+
+    // What happens if no name prop is passed to the Welcome component? Can you set a default value for the name prop?
+    // Non stamperà nulla nel nostro HTML, ma se avessimo impostato un valore predefinito ad esempio "Guest" stampera Guest come output
+    
 }
