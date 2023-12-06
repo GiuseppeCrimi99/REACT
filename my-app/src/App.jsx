accc3f4f8823ee2ad725ccc77548780726fc80f3
Giuseppe Crimi <giuseppecrimi1999@hotmail.com>
2023-12-06 14:21:26 +0100

    exercise of component3

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,25 @@
 import { Hello } from "./Hello";
 import { Message } from "./Message";
 
+
+// È possibile utilizzare il Hello componente più di una volta nel Appcomponente? Cosa succede se lo fai?
+// Si è possibile, chiameremo la nostra funnzione due volte all'interno del nostro html, e vedremo stampato  due volte "Hello Wolrd"
+
+
+// Puoi eseguire il rendering del Messagecomponente direttamente all'interno del Appcomponente? Cosa succede se lo fai?
+// Non capisco come devo fare il secondo punto! 
+
+
+   
+
 export function App (){
     return(
+
         <div>
-        
                  <Hello />
                  <Message/>
+                 <Hello/>  
+
             
            
         </div>
