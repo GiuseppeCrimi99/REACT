c37c72391c319f590f987f10ec366513aeecc73b
Giuseppe Crimi <giuseppecrimi1999@hotmail.com>
2023-12-06 13:27:01 +0100

    exercise of component2

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,12 @@
 import { Hello } from "./Hello";
+import { Message } from "./Message";
 
 export function App (){
     return(
         <div>
         
                  <Hello />
+                 <Message/>
             
            
         </div>
--- my-app/src/index.jsx
+++ my-app/src/index.jsx
@@ -1,5 +1,6 @@
 import { Hello } from "./Hello"
 import { App } from "./App"
+import { Message } from "./Message"
 import {createRoot} from "react-dom/client"
 const rootElement= document.getElementById("root")
 const root= createRoot(rootElement)
