d05a7dd5360d9d38853b608ec59b048241ceba83
Giuseppe Crimi <giuseppecrimi1999@hotmail.com>
2023-12-07 12:27:46 +0100

    second exercise of render conditional

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,7 +4,7 @@ import { Welcome } from "./Welcome";
 export function App (){
     return (
         <div>
-          <Welcome name="John" age={50} />
+          <Welcome name="John" age={10} />
         </div>
       );
     }
--- my-app/src/Welcome.jsx
+++ my-app/src/Welcome.jsx
@@ -5,7 +5,7 @@ export function Welcome({ name, age }) {
     <div>
       {name === "John" && <p>Welcome {name}</p>}
       {!!age && age > 18 && age && age < 65 && <p>You are {age} years old</p>}
-      {age && age < 18 && <></>}
+      {age && age < 18 && <p>You are very young</p>}
 
       
     </div>
