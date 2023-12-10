35d25724a121b120da5c8c23a430484b804b2364
Giuseppe Crimi <giuseppecrimi1999@hotmail.com>
2023-12-10 12:15:47 +0100

    secondo esercizio gestioneEventi

--- my-app/src/MouseClicker.jsx
+++ my-app/src/MouseClicker.jsx
@@ -1,13 +1,21 @@
+import React from "react";
+
 export function MouseClicker(){
 
     function Click(event){
-        console.log(event.target.name);
+        console.log(event.currentTarget.name);
     }
 
+    function ClickImage(event) {
+        console.log(event.currentTarget.src);
+        event.stopPropagation(); // Impedisce la propagazione dell'evento al pulsante
+    }
     return(
-        <>
-            <button name="one"onClick={Click}>Cliccami!</button>
         
-        </>
+        <button name="one" onClick={Click}>
+        <img src="ef.com" alt="" width={30} height={50} onClick={ClickImage} />
+        Cliccami!
+    </button>
+        
     )
 }
