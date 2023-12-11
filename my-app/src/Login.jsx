a6b93223ad6533d150d2ed3968fbdff6d2cc0eb3
Giuseppe Crimi <giuseppecrimi1999@hotmail.com>
2023-12-11 16:52:10 +0100

    third exercise of ctrlform

--- my-app/src/Login.jsx
+++ my-app/src/Login.jsx
@@ -10,15 +10,15 @@ import { useState } from "react"
         }
     }
 
-        export function Login(){
+        export function Login(onLogin){
             const [data, setData]=useState(createData())
-
+             
             function handleImput(event){
                 const name= event.target.name;
                 const value= event.target.value;
 
-
-
+               
+                
 
                 setData((data) => ({
                     ...data,
@@ -27,15 +27,19 @@ import { useState } from "react"
                 }
               
 
+                            
+                function isLoginDisabled() {
+                    return !data.username || !data.password;
+                  }
 
-
-
+             
 
     return(
         <>
             <input name="username"value={data.username}onChange={handleImput} />
             <input name="password"type="password"value={data.password}onChange={handleImput} />
             <input name="checkbox"type="checkbox" value={data.checkbox}onChange={handleImput}/>
+            <button disabled={isLoginDisabled()}>Login!</button>
         
         </>
     )
