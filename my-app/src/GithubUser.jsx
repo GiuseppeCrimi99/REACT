16d767e38c22d0f169dd60fab6c952c4cf93dc4f
Giuseppe Crimi <giuseppecrimi1999@hotmail.com>
2023-12-27 12:34:00 +0100

    fourth exercise of customhook4

--- my-app/src/GithubUser.jsx
+++ my-app/src/GithubUser.jsx
@@ -2,11 +2,12 @@ import { useEffect, useState } from "react"
 import useGitHubUser from "./useGithubUser"
 
  const GithubUser=({username})=>{
-  const{data,error}= useGitHubUser(username)
+  const{data,error,loading}= useGitHubUser(username)
 
     return(
         <div>
             {error&& <h1>There has been an error!!</h1>}
+            {loading&&<h1>Loading...</h1>}
             {data&&<h1>{data.name}</h1>}
             {data&&<p>{data.login}</p>}
             {data&&<img src={data.avatar_url}></img>}
--- my-app/src/useGithubUser.jsx
+++ my-app/src/useGithubUser.jsx
@@ -1,27 +1,29 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-const useGitHubUser=(username)=>{
-    const [data,setData]=useState(null)
-    const [error,setError]=useState(false)
-    const fetchGithubUser=async(username)=>{
-        try {
-        const response= await fetch(`https://api.github.com/users/${username}`)
-        const responseJSON= await response.json()
-        setData(responseJSON)
-        } catch (error) {
-            setError(error)
-            setData(null)
-        }
-    
-    
+const useGitHubUser = (username) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const fetchGithubUser = async (username) => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(`https://api.github.com/users/${username}`);
+      const responseJSON = await response.json();
+      setData(responseJSON);
+    } catch (error) {
+      setError(error);
+      setData(null);
+    } finally {
+      setLoading(false);
     }
+  };
 
+  useEffect(() => {
+    fetchGithubUser(username);
+  }, [username]);
 
-        useEffect(()=>{
-            fetchGithubUser(username)
-        },[username])
-
-        return { data, error }
-
-}
-export default useGitHubUser
+  return { data, error, loading };
+};
+export default useGitHubUser;
